0a9ad4c955901d2bf158504194f52f3fff4235f6
Brian Lee <briancmlee23@gmail.com>
2020-06-13 00:33:56 +1000

    Updates

---
+++ precache-manifest.2e0ce99f25d3aa4c4ac8906fce110ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1951dc1ef4c81b2e1f1a1ca3f301d510",
+    "url": "/vce-softdev-folio-4/index.html"
+  },
+  {
+    "revision": "4ddd6e120af3e674b397",
+    "url": "/vce-softdev-folio-4/static/css/main.75b1ccba.chunk.css"
+  },
+  {
+    "revision": "9523c06e97116db77423",
+    "url": "/vce-softdev-folio-4/static/js/2.0036d8d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vce-softdev-folio-4/static/js/2.0036d8d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddd6e120af3e674b397",
+    "url": "/vce-softdev-folio-4/static/js/main.fd0ac52a.chunk.js"
+  },
+  {
+    "revision": "6e1edb11b5685c4ea737",
+    "url": "/vce-softdev-folio-4/static/js/runtime-main.e84c92be.js"
+  }
+]);
